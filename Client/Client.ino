fa5139fa065ff6fae6647d738c11521f173f91e7
husseinkareem <husseinkeldoon@yahoo.com>
2023-01-03 15:13:43 +0100

    Wifi and Led test

---
+++ Client/Client.ino
@@ -0,0 +1,46 @@
+#include <WiFi.h>
+#include <Adafruit_NeoPixel.h>
+
+#define NUMPIXELS 1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+int red = 0xFF0000;
+int yellow = 0xFFFF00;
+int green = 0x00FF00;
+int currentcolor = green; 
+
+const char* ssid = "NETGEAR88";
+const char* password = "XXXXXXXX";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi"); 
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\nConnected to the WiFi network"); 
+  Serial.print("IP address: "); 
+  Serial.println(WiFi.localIP());
+
+#if defined(NEOPIXEL_POWER)
+ pinMode(NEOPIXEL_POWER, OUTPUT);
+ digitalWrite(NEOPIXEL_POWER, HIGH);
+
+#endif
+ pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+ pixels.setBrightness(10); // not so bright
+}
+void loop() {
+  if ((WiFi.status() == WL_CONNECTED)) {
+    Serial.println("You can try to ping me"); 
+    pixels.fill(currentcolor);
+    pixels.show();
+  }
+  else{
+    Serial.println("Connection lost"); 
+  }
+  delay(1000); 
+}
