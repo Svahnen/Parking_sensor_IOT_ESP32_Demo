7e0f968e2904d2f2aecb0050a2b8376f7c5265ed
Svahnen <svahn.jonny@gmail.com>
2023-01-04 16:22:59 +0100

    Fix rest API and JSON but break sensor

--- Server/Server.ino
+++ Server/Server.ino
@@ -1,74 +1,114 @@
-#include "WiFi.h"
-#include <WebServer.h>
-#include <ArduinoJson.h>
+#include <WiFi.h>
 
+// Replace with your network credentials
 const char* ssid = "NETGEAR88";
-const char* password = "XXXXXXXXXXXX";
+const char* password = "xxxxxxxxxxxxxx";
+WiFiServer server(80);
 
+long duration, distanceCm;
 const int TRIG_PIN = 18;
 const int ECHO_PIN = 17;
-long duration, distanceCm;
 
-WebServer server(80);
-
-// JSON data buffer
-StaticJsonDocument<250> jsonDocument;
-char buffer[250];
-void create_json(char *tag, float value) {  
-  jsonDocument.clear();
-  jsonDocument["type"] = tag;
-  jsonDocument["value"] = value;
-  serializeJson(jsonDocument, buffer);  
-}
- 
-void add_json_object(char *tag, float value) {
-  JsonObject obj = jsonDocument.createNestedObject();
-  obj["type"] = tag;
-  obj["value"] = value;
-}
-
-
-void getDistance() {
-  create_json("distance", distanceCm);
-  server.send(200, "application/json", buffer);
-}
-
+// Client variables
+char linebuf[80];
+int charcount = 0;
 void setup() {
-  // initialize serial communication:
-  Serial.begin(9600);
-  pinMode(TRIG_PIN,OUTPUT);
-  pinMode(ECHO_PIN,INPUT);
 
-  // Wifi
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi"); 
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println("\nConnected to the WiFi network"); 
-  Serial.print("IP address: "); 
-  Serial.println(WiFi.localIP());
+    Serial.begin(9600);
+    pinMode(TRIG_PIN,OUTPUT);
+    pinMode(ECHO_PIN,INPUT);
 
-  server.on("/sensor", getDistance);
-  // start server
-  server.begin();
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    // attempt to connect to Wifi network:
+    while (WiFi.status() != WL_CONNECTED) {
+        // Connect to WPA/WPA2 network.
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
 }
+void loop() {
 
-void loop()
-{
- // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
- digitalWrite(TRIG_PIN, LOW);
- delayMicroseconds(2);
- digitalWrite(TRIG_PIN, HIGH);
- delayMicroseconds(10);
- digitalWrite(TRIG_PIN, LOW);
- duration = pulseIn(ECHO_PIN,HIGH);
- 
- // convert the time into a distance
- distanceCm = duration / 29.1 / 2 ;
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    duration = pulseIn(ECHO_PIN,HIGH);
+    // convert the time into a distance
+    distanceCm = duration / 29.1 / 2 ;
 
- Serial.println(distanceCm);
+    //TODO: Print distance in terminal to see if it works there, we get 0 atm
 
- delay(1000);
+    // listen for incoming clients
+    WiFiClient client = server.available();
+    if (client) {
+        Serial.println("New client");
+        memset(linebuf, 0, sizeof(linebuf));
+        charcount = 0;
+        // an http request ends with a blank line
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+                Serial.write(c);
+                // read char by char HTTP request
+                linebuf[charcount] = c;
+                if (charcount < sizeof(linebuf) - 1) charcount++;
+
+                if (c == '\n' && currentLineIsBlank) {
+                    // send a standard http response header
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: application/json");
+                    client.println("Connection: close");  // the connection will be closed after completion of the response
+                    client.println();
+                    client.println("{");
+
+                    char distanseStr[100];
+                    char distanceJson[] = "\"distance\": ";
+
+                    Serial.print("distanceCm: ");
+                    Serial.println(distanceCm);
+
+                    sprintf(distanseStr, "%ld", distanceCm);
+                    
+                    strcat(distanceJson,distanseStr);
+
+                    Serial.print("distanceJson: ");
+                    Serial.println(distanceJson);
+
+                    client.println(distanceJson);
+                    client.println("}");
+                    break;
+                }
+                if (c == '\n') {
+                    // you're starting a new line
+                    currentLineIsBlank = true;
+                    memset(linebuf, 0, sizeof(linebuf));
+                    charcount = 0;
+
+                } else if (c != '\r') {
+                    // you've gotten a character on the current line
+                    currentLineIsBlank = false;
+                }
+            }
+        }
+        // give the web browser time to receive the data
+        delay(1);
+        // close the connection:
+        client.stop();
+        Serial.println("client disconnected");
+    }
 }
